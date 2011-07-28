64dc43a5feacf641a69403752ace50c0c46f6327
Bryan Stitt <bryan@stitthappens.com>
2011-07-28 13:32:09 -0700

    reorder ssh aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,17 +2,20 @@
 alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
 
 # ssh
-alias asucd-jolt='ssh appark@sv.ucdavis.edu'
-alias asucd-cm='ssh appark@169.237.101.248'
-alias asucd-dev='ssh web@asucd-dev.ucdavis.edu'
-alias asucd-web='ssh web@asucd-web.ucdavis.edu'
-alias asucd-test='ssh web@169.237.101.228'
-alias asucd-mysql='ssh web@asucd-mysql.ucdavis.edu'
-alias asucd-gatekeeper='ssh appark@169.237.101.254 -p 32'
-alias uni-dev='ssh web@unitrans-dev.ucdavis.edu'
-alias uni-web='ssh web@unitrans.com'
 alias asucd-backup='ssh appark@asucd-backup.ucdavis.edu'
+alias asucd-cm='ssh web@169.237.101.248'
+alias asucd-dev='ssh web@asucd-dev.ucdavis.edu'
+alias asucd-elections='ssh appark@elections.ucdavis.edu'
+alias asucd-gatekeeper='ssh appark@169.237.101.254 -p 32'
+alias asucd-jolt='ssh appark@sv.ucdavis.edu'
+alias asucd-mysql='ssh web@asucd-mysql.ucdavis.edu'
+alias asucd-stormtrooper='ssh appark@stormtrooper.ucdavis.edu'
+alias asucd-test='ssh web@169.237.101.228'
+alias asucd-web='ssh web@asucd-web.ucdavis.edu'
+
+alias uni-dev='ssh web@unitrans-dev.ucdavis.edu'
 alias uni-olduws='ssh admin@olduws.ucdavis.edu'
+alias uni-web='ssh web@unitrans.com'
 
 # easy ssh to the ASUCD subnet
 # 'asucd 248 -l appark' -> 'ssh 169.237.101.248 -l appark'
