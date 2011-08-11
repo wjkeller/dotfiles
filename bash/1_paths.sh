bb0932dadb1d54d0ec6e28ba7fea038e24e7a495
Rory Neithinger <rory.trent@gmail.com>
2011-08-11 11:59:52 -0700

    add ~/.path.local

--- bash/1_paths.sh
+++ bash/1_paths.sh
@@ -49,5 +49,9 @@ if [ -d ${HOME}/bin ]; then
     export PATH=${HOME}/bin:$PATH
 fi
 
+if [ -e ${HOME}/.path.local ]; then
+  source ${HOME}/.path.local
+fi
+
 #export PATH="~/bin:~/.bin:/usr/local/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
 #export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
